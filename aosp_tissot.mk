8528a55cdd1c563d3381ca7f7ea72feb1655f4ed
hklknz <haikalluthfianinobaluqia598@gmail.com>
2023-01-06 12:17:27 +0000

    tissot: Change fp prop to Walleye
    Signed-off-by: hklknz <haikalluthfianinobaluqia598@gmail.com>

--- aosp_tissot.mk
+++ aosp_tissot.mk
@@ -36,10 +36,10 @@ TARGET_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
+BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
 
 # stuffs
 TARGET_BOOT_ANIMATION_RES := 1080
