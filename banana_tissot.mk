de07e391073cf6016111a8d7f56cb211a5f38bab
hklknz <haikalluthfianinobaluqia598@gmail.com>
2023-01-29 10:29:38 +0000

    tissot: Update Fp To Raven Tiramisu
    Signed-off-by: hklknz <haikalluthfianinobaluqia598@gmail.com>

--- banana_tissot.mk
+++ banana_tissot.mk
@@ -36,10 +36,10 @@ TARGET_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 13 TQ1A.230105.002 9325679 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:13/TQ1A.230105.002/9325679:user/release-keys"
 
 # stuffs
 TARGET_BOOT_ANIMATION_RES := 1080
