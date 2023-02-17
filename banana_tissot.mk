420c4795900c201e769679b406e76f328ffc66be
hklknz <haikalluthfianinobaluqia598@gmail.com>
2023-02-17 08:47:25 +0000

    tissot: Bringback to stock oreo fp Mi A1
    Signed-off-by: hklknz <haikalluthfianinobaluqia598@gmail.com>

--- banana_tissot.mk
+++ banana_tissot.mk
@@ -36,10 +36,10 @@ TARGET_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 13 TQ1A.230105.002 9325679 release-keys"
+    PRIVATE_BUILD_DESC="tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/raven/raven:13/TQ1A.230105.002/9325679:user/release-keys"
+BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
 
 # stuffs
 TARGET_BOOT_ANIMATION_RES := 1080
