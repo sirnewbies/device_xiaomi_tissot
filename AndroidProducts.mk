9e30d5dd0f37dc2aa6974cdc3ae87b86422c0c83
Anothermi1 <yusromi04@gmail.com>
2023-04-05 06:16:48 +0700

    tissot: bringup hentai

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_tissot.mk
+    $(LOCAL_DIR)/hentai_tissot.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_tissot-user \
-    aosp_tissot-userdebug \
-    aosp_tissot-eng
+    hentai_tissot-user \
+    hentai_tissot-userdebug \
+    hentai_tissot-eng
