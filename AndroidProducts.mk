096abff9b89353d69dfacd73eacb546af58cec76
Anothermi1 <yusromi04@gmail.com>
2023-04-04 21:18:28 +0700

    tissot: bringup aosp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_tissot.mk
+    $(LOCAL_DIR)/aosp_tissot.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_tissot-user \
-    lineage_tissot-userdebug \
-    lineage_tissot-eng
+    aosp_tissot-user \
+    aosp_tissot-userdebug \
+    aosp_tissot-eng
--- aosp_tissot.mk
+++ aosp_tissot.mk
@@ -22,11 +22,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/tissot/device.mk)
 
 # Inherit some common RiceDroid stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tissot
-PRODUCT_NAME := lineage_tissot
+PRODUCT_NAME := aosp_tissot
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A1
@@ -35,13 +35,11 @@ TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# RiceDroid Stuffs
-RICE_MAINTAINER := ZHANtech™
-RICE_DEVICE := tissot
+# Stuffs
+DEVICE_MAINTAINER := Romi Yusnandar
 TARGET_FACE_UNLOCK_SUPPORTED := true
 USE_AOSP_CLOCK := true
 SUSHI_BOOTANIMATION := 1080
-TARGET_INCLUDE_STOCK_ARCORE := false
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
 TARGET_SUPPORTS_QUICK_TAP := true
@@ -52,17 +50,6 @@ TARGET_BUILD_APERTURE_CAMERA := true
 
 # disable/enable blur support, default is false
 TARGET_ENABLE_BLUR := true
-
-# GMS build flags, if none were defined the package build type will be AOSP (default: false)
-WITH_GMS := true
-
-# Opt out of google dialer support, compiler will build aosp dialer,
-# package type will change from PIXEL -> GMS
-TARGET_OPTOUT_GOOGLE_TELEPHONY := true
-
-# Compiler will only build GMS playstore services, its dependencies, and Gboard app.
-# package type will change from PIXEL/GMS -> CORE
-TARGET_CORE_GMS := true
 
 # Spoof build description/fingerprint as pixel device
 TARGET_USE_PIXEL_FINGERPRINT := true
